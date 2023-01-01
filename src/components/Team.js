f06071ca8f9a42b23f0ab5a899f3c00c17c9b6c8
DZ-staking <112197568+FCPIDZ213@users.noreply.github.com>
2023-01-01 19:17:12 +0100

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -19,17 +19,17 @@ export const Team = () => {
       imgUrl: projImg1,
 
     },
-    {
+    {/*
       title: "",
       description: "",
       imgUrl: projImg2,
-    },
+    */},
     {
       title: "G-tech",
       description: "Co-founder",
       imgUrl: projImg4,
     },
-    {
+    {/*
       title: "",
       description: "",
       imgUrl: projImg3,
@@ -38,7 +38,7 @@ export const Team = () => {
       title: "",
       description: "",
       imgUrl: projImg5,
-    },
+    */},
  
   ];
 
