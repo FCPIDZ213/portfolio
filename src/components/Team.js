f1600ce342b237017b4a24462a8c0cd275af6878
DZ-staking <112197568+FCPIDZ213@users.noreply.github.com>
2023-01-01 18:53:27 +0100

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -7,8 +7,8 @@ import projImg3 from "../assets/img/fond.png";
 import projImg4 from "../assets/img/team2.png";
 import projImg5 from "../assets/img/fond.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+/* import 'animate.css';
+import TrackVisibility from 'react-on-screen'; */
 
 export const Team = () => {
 
