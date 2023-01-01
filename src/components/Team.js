ed01aeae733b2fc7f3c10e57b06dce2659dc33a3
DZ-staking <112197568+FCPIDZ213@users.noreply.github.com>
2023-01-01 18:34:07 +0100

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,10 +3,10 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { TeamCard } from "./TeamCard";
 import projImg1 from "../assets/img/nodex.png";
 import projImg2 from "../assets/img/team2.png";
-import projImg3 from "../assets/img/team3.png";
-import projImg4 from "../assets/img/team4.png";
-import projImg5 from "../assets/img/team5.png";
-import colorSharp2 from "";
+import projImg3 from "../assets/img/team9.png";
+import projImg4 from "../assets/img/team7.png";
+import projImg5 from "../assets/img/team6.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
