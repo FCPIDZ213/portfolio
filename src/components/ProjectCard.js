bd664519dd731aa35f0a0f99a74d71cc39ad0b58
DZ-staking <112197568+FCPIDZ213@users.noreply.github.com>
2023-01-01 18:48:34 +0100

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+/* export const ProjectCard = ({ title, description, imgUrl }) => { */
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
