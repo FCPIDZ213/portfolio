dbb6e70e2883666987755e0c352c69bcdb04d6b5
DZ-staking <112197568+FCPIDZ213@users.noreply.github.com>
2023-01-01 04:40:18 +0100

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { TeamCard } from "./TeamCard";
 import projImg1 from "../assets/img/nodex.png";
-
+import projImg2 from "../assets/img/faz.png";
 import projImg3 from "../assets/img/team9.png";
+import projImg4 from "../assets/img/team7.png";
+import projImg5 from "../assets/img/team6.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -17,13 +19,27 @@ export const Team = () => {
       imgUrl: projImg1,
 
     },
-    
+    {/*
+      title: "FazNode",
+      description: "Core Team",
+      imgUrl: projImg2,
+    */},
     {
       title: "G-tech",
       description: "Co-founder",
       imgUrl: projImg4,
     },
-        
+    {/*
+      title: "BlackRock",
+      description: "Core Team",
+      imgUrl: projImg3,
+    */},
+    {/*
+      title: "CryptoPunk",
+      description: "Core Team",
+      imgUrl: projImg5,
+    */},
+    
   ];
 
   return (
